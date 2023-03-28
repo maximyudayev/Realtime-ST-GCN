dce54547b2cf139be440c46c29f5ff122a688883
maximyudayev <maxim.yudayev@gmail.com>
2023-03-28 12:56:46 +0200

    Added F1@k and segmental edit metrics
    +: F1@k and segmental Levenshtein distance calculation in
       `benchmark`.
    
    -: Latency capturing in `benchmark`.
    
    TODO: Move latency capture to a separate procedure.

--- processor.py
+++ processor.py
@@ -585,77 +585,216 @@ class Processor:
         NOTE: currently only supports `original` and `realtime` on `dir` dataset types.
         """
 
-        if kwargs['model'] == 'realtime':
-            self.model._swap_layers_for_inference()
-
         # set layers to inference mode if behavior differs between train and prediction
         # (prepares Dropout and BatchNormalization layers to enable and to freeze parameters, respectively)
         self.model.eval()
 
+        # move the model to the compute device(s) if available (CPU, GPU, TPU, etc.)
+        if torch.cuda.device_count() > 1:
+            print("Using", torch.cuda.device_count(), "allocated GPUs", flush=True, file=kwargs['log'][0])
+            self.model = nn.DataParallel(self.model)
+        self.model.to(device)
+
         # do not record gradients
         with torch.no_grad():
-            latency = 0
+            top1_correct = 0
+            top5_correct = 0
+            total = 0
+
+            # confusion matrix for frame-wise metrics
+            confusion_matrix = torch.zeros(self.num_classes, self.num_classes, device=device, dtype=torch.int64)
+            
+            # stats for F1@k segmentation-detection metric of Lea, et al. (2016)
+            overlap = torch.tensor([0.1, 0.25, 0.5], device=device, dtype=torch.float32)
+            f1 = torch.zeros(len(dataloader), overlap.size(0), device=device, dtype=torch.float32)
+            
+            # stats for segmental edit score
+            edit_score = 0
 
             # sweep through the training dataset in minibatches
-            for i, (captures, labels) in enumerate(dataloader):
+            # calculate IoU for the F1@k metrics
+            # NOTE: (assumes N=1)
+            for j, (captures, labels) in enumerate(dataloader):
+                top1_predicted, _, top1_cor, top5_cor, tot, _, _ = self.forward_(captures, labels, device, **kwargs)
+
+                top1_correct += top1_cor
+                top5_correct += top5_cor
+                total += tot
+
+                labels = labels.to(device)
+                N, L = labels.size()
+
+                # detects edges of ground truth
+                # (will yield non-zero values at the edges of classes)
+                edges_labels = torch.zeros(L, device=device, dtype=torch.int64)
+                edges_labels[0] = 1
+                edges_labels[1:] = labels[0,1:]-labels[0,:-1]
+                
+                edges_indices_labels = edges_labels.nonzero()[:,0]
+                edges_indices_labels_shifted = torch.zeros_like(edges_indices_labels, device=device, dtype=torch.int64)
+                edges_indices_labels_shifted[:-1] = edges_indices_labels[1:]
+                edges_indices_labels_shifted[-1] = L
+
+                label_segments_used = torch.zeros(edges_indices_labels.size(0), overlap.size(0), device=device, dtype=torch.bool)
+
+                # detects edges of predictions
+                edges_predictions = torch.zeros(L, device=device, dtype=torch.int64)
+                edges_predictions[0] = 1
+                edges_predictions[1:] = top1_predicted[0,1:]-top1_predicted[0,:-1]
+
+                edges_indices_predictions = edges_predictions.nonzero()[:,0]
+                edges_indices_predictions_shifted = torch.zeros_like(edges_indices_predictions, device=device, dtype=torch.int64)
+                edges_indices_predictions_shifted[:-1] = edges_indices_predictions[1:]
+                edges_indices_predictions_shifted[-1] = L
+
+                tp = torch.zeros(self.num_classes, overlap.size(0), device=device, dtype=torch.int64)
+                fp = torch.zeros(self.num_classes, overlap.size(0), device=device, dtype=torch.int64)
+
+                # check every segment of predictions for overlap with ground truth
+                # segment as a whole is marked as TP/FP/FN, not frame-by-frame
+                # earliest correct prediction, for a given ground truth, will be marked TP
+                # mark true positive segments (first correctly predicted segment exceeding IoU threshold)
+                # mark false positive segments (all further correctly predicted segments exceeding IoU threshold, or those under it)
+                # mark false negative segments (all not predicted actual frames)
+                for i in range(edges_indices_predictions.size(0)):
+                    intersection = torch.minimum(edges_indices_predictions_shifted[i],edges_indices_labels_shifted) - torch.maximum(edges_indices_predictions[i],edges_indices_labels)
+                    union = torch.maximum(edges_indices_predictions_shifted[i],edges_indices_labels_shifted) - torch.minimum(edges_indices_predictions[i],edges_indices_labels)
+                    # IoU is valid if the predicted class of the segment corresponds to the actual class of the overlapped ground truth segment
+                    IoU = (intersection/union)*(top1_predicted[0,edges_indices_predictions[i]]==labels[0,edges_indices_labels])
+                    # ground truth segment with the largest IoU is the (potential) hit
+                    idx = IoU.argmax()
+
+                    # predicted segment is a hit if it exceeds IoU threshold and if its label has not been matched against yet
+                    hits = torch.bitwise_and(IoU[idx].gt(overlap),torch.bitwise_not(label_segments_used[idx]))
+
+                    # mark TP and FP correspondingly
+                    # correctly classified, exceeding the threshold and the first predicted segment to match the ground truth
+                    tp[top1_predicted[0,edges_indices_predictions[i]]] += hits
+                    # correctly classified, but under the threshold or not the first predicted segment to match the ground truth
+                    fp[top1_predicted[0,edges_indices_predictions[i]]] += torch.bitwise_not(hits)
+                    # mark ground truth segment used if marked TP
+                    label_segments_used[idx] += hits
+
+                TP = tp.sum(dim=0)
+                FP = fp.sum(dim=0)
+                # FN are unmatched ground truth segments (misses)
+                FN = label_segments_used.size(0) - label_segments_used.sum(dim=0)
+
+                # calculate the F1 score
+                precision = TP / (TP+FP)
+                recall = TP / (TP+FN)
+                f1[j] = 2*precision*recall/(precision+recall)
+
+                # collect the segmental edit score
+                m_row = edges_indices_predictions.size(0)
+                n_col = edges_indices_labels.size(0)
+
+                D = torch.zeros(m_row+1, n_col+1, device=device, dtype=torch.float32)
+                D[:,0] = torch.arange(m_row+1)
+                D[0,:] = torch.arange(n_col+1)
+
+                for j in range(1, n_col+1):
+                    for i in range(1, m_row+1):
+                        if labels[0,edges_indices_labels][j-1] == top1_predicted[0,edges_indices_predictions][i-1]:
+                            D[i, j] = D[i - 1, j - 1]
+                        else:
+                            D[i, j] = min(D[i - 1, j] + 1,
+                                        D[i, j - 1] + 1,
+                                        D[i - 1, j - 1] + 1)
+
+                edit_score += (1 - D[-1, -1] / max(m_row, n_col))
+
+                # collect the correct predictions for each class and total per that class
+                # for batch_el in range(N*M):
+                for batch_el in range(N):
+                    # OHE 3D matrix, where label and prediction at time `t` are indices
+                    top1_ohe = torch.zeros(L, self.num_classes, self.num_classes, device=device, dtype=torch.bool)
+                    top1_ohe[range(L), top1_predicted[batch_el], labels[batch_el]] = True
+
+                    # sum-reduce OHE 3D matrix to get number of true vs. false classifications for each class on this sample
+                    confusion_matrix += torch.sum(top1_ohe, dim=0)
+
+            top1_acc = top1_correct / total
+            top5_acc = top5_correct / total
+            # discard NaN F1 values and compute the macro F1-score (average)
+            F1 = f1.nan_to_num(0).mean(dim=0)
+            edit_score /= len(dataloader)
+
+            pd.DataFrame(torch.stack((overlap,F1)).cpu().numpy()).to_csv('{0}/macro-F1@k.csv'.format(save_dir))
+            pd.DataFrame(data={"top1": top1_acc, "top5": top5_acc}, index=[0]).to_csv('{0}/accuracy.csv'.format(save_dir))
+            pd.DataFrame(data={"edit": edit_score.cpu().numpy()}, index=[0]).to_csv('{0}/edit.csv'.format(save_dir))
+            # save confusion matrix as a CSV file
+            pd.DataFrame(confusion_matrix.cpu().numpy()).to_csv('{0}/confusion_matrix.csv'.format(save_dir))
+
+            # sweep through the sample trials
+            for i in [175, 293, 37]: # 39 (L=4718), 177 (L=6973), 299 (L=2378)
+                # save prediction and ground truth of reference samples
+                captures, labels = dataloader.dataset.__getitem__(i)
+
                 # move both data to the compute device
                 # (captures is a batch of full-length captures, label is a batch of ground truths)
-                captures, labels = captures.to(device), labels.to(device)
+                captures, labels = captures[None].to(device), labels[None].to(device)
 
                 N, _, L, _ = captures.size()
-                lat = 0
 
-                # Splits trial for `original` model into overlapping subsequences of samples to separately feed into the model
-                if kwargs['model'] == 'original':
-                    # zero pad the input across time from start by the receptive field size
-                    captures = F.pad(captures, (0, 0, kwargs['receptive_field']-1, 0))
-                    stride = kwargs['receptive_field'] if kwargs['latency'] else 1
-                    captures = captures.unfold(2, kwargs['receptive_field'], stride)
-                    labels = labels[:, ::stride]
+                top1_predicted, _, _, _, _, _, _ = self.forward_(captures, labels, device, **kwargs)
+
+                pd.DataFrame(torch.stack((labels[0], top1_predicted[0])).cpu().numpy()).to_csv('{0}/segmentation-{1}.csv'.format(save_dir, i))
+
+            # # timing benchmarking
+            # latency = 0
+            # if kwargs['model'] == 'realtime':
+            #     self.model._swap_layers_for_inference()
+
+            # # Splits trial for `original` model into overlapping subsequences of samples to separately feed into the model
+            # if kwargs['model'] == 'original':
+            #     # zero pad the input across time from start by the receptive field size
+            #     captures = F.pad(captures, (0, 0, kwargs['receptive_field']-1, 0))
+            #     stride = kwargs['receptive_field'] if kwargs['latency'] else 1
+            #     captures = captures.unfold(2, kwargs['receptive_field'], stride)
+            #     labels = labels[:, ::stride]
+            
+            #     N, C, N_new, V, T_new = captures.size()
+            #     # (N,C,N',V,T') -> batches of unfolded slices
+            #     # .contiguous() is needed before .view(), but also after .unfold() to operate on same data element 
+            #     # in the overlapping segments. Otherwise two segments will update the same memory location, leaking data.
+            #     # No need to .clone() the sliced view of the tensor after .contiguous()
+            #     captures = captures.permute(0, 2, 1, 4, 3).contiguous()
+            #     captures = captures.view(N * N_new, C, T_new, V)
+            #     # (N'',C,T',V)
+
+            #     with cProfile.Profile() as pr:
+            #         for j in range(N_new):
+            #             start = time.time()
+
+            #             # the input tensor has shape (N, V, C, L): N-batch, V-nodes, C-channels, L-length
+            #             # the output tensor has shape (N, C', L)
+            #             self.model(captures[j:j+1])
+                        
+            #             lat += (time.time() - start)
+
+            #         pstats.Stats(pr).sort_stats(SortKey.TIME).print_stats()
+
+            #     latency += lat / N_new
+            # else:
+            #     with cProfile.Profile() as pr:
+            #         for j in range(L):
+            #             start = time.time()
+
+            #             # the input tensor has shape (N, V, C, L): N-batch, V-nodes, C-channels, L-length
+            #             # the output tensor has shape (N, C', L)
+            #             self.model(captures[:,:,j:j+1,:])
+                        
+            #             lat += (time.time() - start)
                 
-                    N, C, N_new, V, T_new = captures.size()
-                    # (N,C,N',V,T') -> batches of unfolded slices
-                    # .contiguous() is needed before .view(), but also after .unfold() to operate on same data element 
-                    # in the overlapping segments. Otherwise two segments will update the same memory location, leaking data.
-                    # No need to .clone() the sliced view of the tensor after .contiguous()
-                    captures = captures.permute(0, 2, 1, 4, 3).contiguous()
-                    captures = captures.view(N * N_new, C, T_new, V)
-                    # (N'',C,T',V)
+            #         pstats.Stats(pr).sort_stats(SortKey.TIME).print_stats()
 
-                    with cProfile.Profile() as pr:
-                        for j in range(N_new):
-                            start = time.time()
+            #     latency += lat / L
 
-                            # the input tensor has shape (N, V, C, L): N-batch, V-nodes, C-channels, L-length
-                            # the output tensor has shape (N, C', L)
-                            self.model(captures[j:j+1])
-                            
-                            lat += (time.time() - start)
-
-                        pstats.Stats(pr).sort_stats(SortKey.TIME).print_stats()
-
-                    latency += lat / N_new
-                else:
-                    with cProfile.Profile() as pr:
-                        for j in range(L):
-                            start = time.time()
-
-                            # the input tensor has shape (N, V, C, L): N-batch, V-nodes, C-channels, L-length
-                            # the output tensor has shape (N, C', L)
-                            self.model(captures[:,:,j:j+1,:])
-                            
-                            lat += (time.time() - start)
-                    
-                        pstats.Stats(pr).sort_stats(SortKey.TIME).print_stats()
-
-                    latency += lat / L
-                
-                break
-
-            print(
-                "[benchmark]: {0} spf"
-                .format(latency/(i+1)),
-                flush=True,
-                file=kwargs['log'][0])
+            # print(
+            #     "[benchmark]: {0} spf"
+            #     .format(latency/(i+1)),
+            #     flush=True,
+            #     file=kwargs['log'][0])
 
         return
