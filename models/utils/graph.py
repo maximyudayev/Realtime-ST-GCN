3bdbbeba969a2bc9f2d14a2a9097773aea81ec95
maximyudayev <maxim.yudayev@gmail.com>
2023-05-12 00:00:12 +0200

    Corrected @yysijie graph construction
    Corrected and verified that adjacency matrix is correctly partitioned
    for `spatial` strategy. Validated by hand examining NTU-RGB+D
    skeleton connections for each partition.
    
    +: Creating correct distance matrix.
    +: Correct partitioning based on new distance matrix.
    
    TODO: Train with normalization after partitioning and before.

--- models/utils/graph.py
+++ models/utils/graph.py
@@ -2,7 +2,13 @@ import numpy as np
 
 class Graph():
     """The Graph to model the node relationships in a skeleton.
+
+    Each row corresponds to a unique node neighborhood: important for normalization and partitioning 
+    because each separate matrix is no longer symmetric after these operations.
     
+    NOTE: $\alpha$ IS needed because "far" adjacency matrix partition in a skeleton
+    will contain 0's for node's at the tip of a limb.
+
     Attributes:
         A : ``NDArray[float64]`` 
             weighted normalized adjacency matrix.
@@ -31,7 +37,8 @@ class Graph():
         center,
         strategy = 'spatial',
         max_hop = 1,
-        dilation = 1):
+        dilation = 1,
+        alpha = 0.001):
         """
         Args:
             num_node : ``int``
@@ -48,6 +55,9 @@ class Graph():
             
             dilation : ``int``
                 controls the spacing between the kernel points.
+
+            alpha : ``int``
+                stability value to avoid empty rows in partitioned adjacency matrix.
             
             strategy : ``string``
                 must be one of the following:
@@ -64,6 +74,7 @@ class Graph():
         self.num_node = num_node
         self.edge = edge
         self.center = center
+        self.alpha = alpha
 
         self.hop_dis = self.get_hop_distance()
         self.A = self.get_adjacency(strategy)
@@ -101,18 +112,17 @@ class Graph():
         adjacency = np.zeros((self.num_node, self.num_node))
         for hop in valid_hop:
             adjacency[self.hop_dis == hop] = 1
-        normalize_adjacency = self.normalize_undigraph(adjacency)
+        # adjacency = self.normalize_undigraph(adjacency)
 
         if strategy == 'uniform':
             A = np.zeros((1, self.num_node, self.num_node))
-            A[0] = normalize_adjacency
-            return A
         elif strategy == 'distance':
             A = np.zeros((len(valid_hop), self.num_node, self.num_node))
             for i, hop in enumerate(valid_hop):
-                A[i][self.hop_dis == hop] = normalize_adjacency[self.hop_dis == hop]
-            return A
+                A[i][self.hop_dis == hop] = adjacency[self.hop_dis == hop]
         elif strategy == 'spatial':
+            # NOTE: original implementation of Yan, et al. (2018) (https://arxiv.org/abs/1801.07455) 
+            # did not calculate distance in accordance with their proposed strategy: used hop distance to center node, not average spatial distance
             A = []
             for hop in valid_hop:
                 a_root = np.zeros((self.num_node, self.num_node))
@@ -120,43 +130,56 @@ class Graph():
                 a_far = np.zeros((self.num_node, self.num_node))
                 for i in range(self.num_node):
                     for j in range(self.num_node):
-                        if self.hop_dis[j, i] == hop:
+                        if self.hop_dis[i, j] == hop:
                             if self.hop_dis[j, self.center] == self.hop_dis[i, self.center]:
-                                a_root[j, i] = normalize_adjacency[j, i]
-                            elif self.hop_dis[j, self.center] > self.hop_dis[i, self.center]:
-                                a_close[j, i] = normalize_adjacency[j, i]
+                                a_root[i, j] = adjacency[i, j]
+                            elif (self.hop_dis[j, self.center] < self.hop_dis[i, self.center] or 
+                                (i == self.center and self.hop_dis[j, self.center] > self.hop_dis[i, self.center])):
+                                # COG node and limb ends have only "close" nodes
+                                a_close[i, j] = adjacency[i, j]
                             else:
-                                a_far[j, i] = normalize_adjacency[j, i]
+                                a_far[i, j] = adjacency[i, j]
                 if hop == 0:
                     A.append(a_root)
                 else:
-                    A.append(a_root + a_close)
+                    A.append(a_close)
                     A.append(a_far)
             A = np.stack(A)
-            return A
+            # summing together partitions adds up to the original adjacency matrix
+            # (np.sum(A,0)==adjacency).all()
         else:
             raise ValueError("Strategy Does Not Exist.")
+        
+        # normalization of the adjacency matrix must be done after partitioning
+        for i in range(A.shape[0]):
+            A[i] = self.normalize_undigraph(A[i])
+        return A
 
 
     def get_hop_distance(self):
         """Computes the distance between nodes.        
 
+        TODO: compute all-pairs shortest paths using the edge list instead of the adjacency matrix
+
         Returns:
             Distance square matrix of size of the number of joints.
         """
 
-        A = np.zeros((self.num_node, self.num_node))
+        cost = np.zeros((self.num_node, self.num_node)) + np.inf
         for i, j in self.edge:
-            A[j, i] = 1
-            A[i, j] = 1
+            if i == j:
+                cost[i, i] = 0
+            else:
+                cost[j, i] = 1
+                cost[i, j] = 1
 
-        # compute hop steps
-        hop_dis = np.zeros((self.num_node, self.num_node)) + np.inf
-        transfer_mat = [np.linalg.matrix_power(A, d) for d in range(self.max_hop + 1)]
-        arrive_mat = (np.stack(transfer_mat) > 0)
-        for d in range(self.max_hop, -1, -1):
-            hop_dis[arrive_mat[d]] = d
-        return hop_dis
+        # use BFS to compute the distance matrix from the adjacency matrix
+        for k in range(self.num_node):
+            for i in range(self.num_node):
+                for j in range(self.num_node):
+                    if cost[i,k] + cost[k,j] < cost [i,j]:
+                        cost[i,j] = cost[i,k] + cost[k,j]
+        return cost
 
 
     def normalize_digraph(self, A):
@@ -170,12 +193,9 @@ class Graph():
             Normalized adjacency matrix of the same size.
         """
 
-        Dl = np.sum(A, 0)
-        num_node = A.shape[0]
-        Dn = np.zeros((num_node, num_node))
-        for i in range(num_node):
-            if Dl[i] > 0:
-                Dn[i, i] = Dl[i]**(-1)
+        Dl = np.power(np.sum(A, 1),-1)
+        Dl[np.isinf(Dl)] = 0
+        Dn = np.eye(A.shape[0]) * Dl
         AD = np.dot(A, Dn)
         return AD
 
@@ -191,11 +211,8 @@ class Graph():
             Normalized adjacency matrix of the same size.
         """
 
-        Dl = np.sum(A, 0)
-        num_node = A.shape[0]
-        Dn = np.zeros((num_node, num_node))
-        for i in range(num_node):
-            if Dl[i] > 0:
-                Dn[i, i] = Dl[i]**(-0.5)
+        Dl = np.power(np.sum(A, 1),-0.5)
+        Dl[np.isinf(Dl)] = 0
+        Dn = np.eye(A.shape[0]) * Dl
         DAD = np.dot(np.dot(Dn, A), Dn)
         return DAD
