a77bb46777b1a019a117d6da5d9719b420ca4dcc
aldaonggar <nuraldaonggar@gmail.com>
2024-02-28 15:13:43 +0100

    Made temporal shift with hardcoded shift parameter

--- models/rtstgcn/test_rtstgcn.py
+++ models/rtstgcn/test_rtstgcn.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn.functional as F
+import numpy as np
 
 # Test dimensions
 N = 1
@@ -13,7 +14,11 @@ G = S*(K-1)+1
 D = int(V*(1+V)/2)
 
 # FIFO for internal logic
-fifo = torch.zeros(N,G,C,V,dtype=torch.int64)
+fifo = torch.zeros(N,1,C,V,dtype=torch.int64)
+#Populate the FIFO with the different numbers to see the temporal shift
+for i in range(1, G):
+    fr = torch.Tensor(N,1,C,V).fill_(i)
+    fifo = torch.cat((fr, fifo), 1)
 
 # Makes P V-by-V symmetric adjacency matrices
 A = torch.zeros(P,V,V,dtype=torch.int64)
@@ -53,14 +58,40 @@ outputs = []
 for i in range(e.shape[1]):
     # push the frame into the FIFO
     fifo = torch.cat((e.sum(dim=2)[:,i:i+1], fifo[:,:G-1]), 1)
-    
+
     # slice the tensor according to the temporal stride size
     # (if stride is 1, returns the whole tensor itself)
     f = fifo[:,range(0, G, S)]
+    
+    #Adaptive temporal shift
+    u = 3.5 #Temporal shift parameter
+    num_of_partitions = 2 * u + 1 #Number of partitions
+    partition_size = int(f.shape[2] / num_of_partitions) #Size of each partition
+    shifted_f = torch.reshape(f, [G*C, V]) #Initialize the shifted tensor
+    u = np.ceil(u)
+    shifts = [i for i in range(-int(u), int(u) + 1) if i != 0] #Create the shifts
+    
+    # temp_tensor = torch.eye(C, G*C)
+    # temp_tensor = torch.roll(temp_tensor, -2, 0)
+    # print(temp_tensor[0:G, 0:G])
+    temp_tensor = torch.eye(G*C, G*C)
+    for i in range(G):
+        for j in range(len(shifts)):
+            temp_tensor[:, i * C + (partition_size * j): i * C + (partition_size * (j + 1))] = \
+            torch.roll(temp_tensor[:, i * C + (partition_size * j): i * C + (partition_size * (j + 1))], shifts[j], 0)
+    
+    temp_tensor[G*C - partition_size: G*C, 0:partition_size] = 0
+    temp_tensor[0:partition_size, G*C - partition_size: G*C] = 0
+
+
+    print(f[0][0][59] + f[0][1][3])
+    shifted_f = torch.matmul(temp_tensor, shifted_f).reshape([N, G, C, V])
+    print(shifted_f[0][0][63])      
 
     # sum temporally and across partitions
     g = torch.sum(f, dim=(1))
     outputs.append(g)
+
 
 # stack frame-wise tensors into the original length L
 # [(N,C,V)] -> (N,C,L,V)
@@ -89,4 +120,4 @@ A_temp = A.permute(2,0,1)[:,:,None,:,None]
 
 temp = F.conv3d(a_temp, A_temp).permute(0,4,2,3,1)[:,:,0]
 
-assert(torch.equal(h, temp))
+#assert(torch.equal(h, temp))
